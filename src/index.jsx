27dd88df73a9cad6f6e8ac9451e23fed86bceb88
Pavel <2042_2042@mail.ru>
2023-02-04 13:55:09 +0300

    feat: add redux-persist for save information in localstorage

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +11,9 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
-                <App />
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,14 +1,46 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import displayReducer from './reducers/displayReducer';
 import controlReducer from './reducers/controlReducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
 const rootReducers = combineReducers({
     display: displayReducer,
     control: controlReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
 const store = configureStore({
-    reducer: rootReducers,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
 
+export const persistor = persistStore(store);
 export default store;
