4935dc57fa16ba84d43445fa0221ac40fd819361
Pavel <2042_2042@mail.ru>
2023-02-06 15:46:52 +0300

    refactor: remove styles

--- src/pages/AppFunc.jsx
+++ src/pages/AppFunc.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 import ControlPanel from '@components/AppFuncComponents/ControlPanel/ControlPanel';
 import History from '@components/AppFuncComponents/History/History';
 import Calculator from '@components/AppFuncComponents/Calculator/Calculator';
 import Header from '@components/AppFuncComponents/Header/Header';
-
-const StyledApp = styled.div`
-    height: 100vh;
-    background: ${({ theme }) => theme.background.primary};
-`;
-
-const StyledWrapperApp = styled.div`
-    display: grid;
-    grid-template-areas:
-        'header header header'
-        'display history controls'
-        'keypad history controls';
-    grid-template-rows: 1fr 1fr 7fr;
-    grid-template-columns: 4fr 1fr 1fr;
-    max-width: 1300px;
-    margin: 0 auto;
-    height: 100%;
-`;
+import { StyledApp, StyledWrapperApp } from '@styles/App/style';
 
 const AppFunc = () => {
     return (
