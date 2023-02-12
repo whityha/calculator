590b7e7b45bcd795cad5ff7bab5b0917900a57dd
Pavel <2042_2042@mail.ru>
2023-02-12 16:04:21 +0300

    refactor: change history style. add font Montserrat.

--- src/components/History/styled.js
+++ src/components/History/styled.js
@@ -2,13 +2,25 @@ import styled from 'styled-components';
 
 export const WrapperHistory = styled.div`
     display: flex;
+    position: relative;
     flex-direction: column;
     gap: 50px;
     align-items: center;
     grid-area: history;
     border-left: 2px solid
         ${({ theme: { background } }) => background.secondary};
-    padding: 1rem;
+    padding: 0 20px 50px 20px;
+    overflow-y: auto;
+`;
+
+export const Title = styled.h1`
+    position: sticky;
+    top: 0;
+    background: white;
+    display: block;
+    width: 100%;
+    padding: 30px 30px;
+    text-align: center;
 `;
 
 export const List = styled.ul`
@@ -22,4 +34,5 @@ export const List = styled.ul`
 export const ListItem = styled.li`
     border-top: 1px solid ${({ theme: { background } }) => background.secondary};
     padding: 5px;
+    text-align: center;
 `;
