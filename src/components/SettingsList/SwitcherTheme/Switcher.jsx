b1d7edd9a1220b8b4309d4c7bbf8e1003d283191
Pavel <2042_2042@mail.ru>
2023-02-14 17:14:24 +0300

    refactor: map options

--- src/components/SettingsList/SwitcherTheme/Switcher.jsx
+++ src/components/SettingsList/SwitcherTheme/Switcher.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { switchTheme } from '@actions/controlsActions';
-import { THEMES } from '@styled/theme/config';
 
+import OPTIONS from './config';
 import { Option, Switcher, Title } from './styled';
 
 const SwitcherThemeFC = () => {
     const theme = useSelector(({ control }) => control.theme);
     const dispatch = useDispatch();
-    const { DARK, LIGHT, COLORED } = THEMES;
 
     const handleTheme = (e) => {
         dispatch(switchTheme(e.target.value));
@@ -17,9 +16,12 @@ const SwitcherThemeFC = () => {
         <>
             <Title>Switch theme</Title>
             <Switcher defaultValue={theme} onChange={handleTheme}>
-                <Option value={DARK}>DARK</Option>
-                <Option value={LIGHT}>LIGHT</Option>
-                <Option value={COLORED}>COLORED</Option>
+                {OPTIONS.map(({ id, value }) => (
+                    <Option key={id} value={value}>
+                        {value.toUpperCase()}
+                    </Option>
+                ))}
+                ;
             </Switcher>
         </>
     );
