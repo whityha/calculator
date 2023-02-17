53bcf12e1c0779b32d1ef5127876d905e310ee45
Pavel <2042_2042@mail.ru>
2023-02-17 12:48:20 +0300

    feat: add page NotFound

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { CC_PATH, DEFAULT, FC_PATH, SETTINGS_PATH } from '@constants/paths';
 
+import NotFoundPage from '../pages/NotFound';
+
 const HomeCC = React.lazy(() => import('../pages/Home/HomeCC'));
 const HomeFC = React.lazy(() => import('../pages/Home/HomeFC'));
 const Settings = React.lazy(() => import('../pages/Settings'));
@@ -9,7 +11,7 @@ const ROUTES = [
     { path: CC_PATH, element: <HomeCC /> },
     { path: FC_PATH, element: <HomeFC /> },
     { path: SETTINGS_PATH, element: <Settings /> },
-    { path: DEFAULT, element: <HomeFC /> },
+    { path: DEFAULT, element: <NotFoundPage /> },
 ];
 
 export default ROUTES;
